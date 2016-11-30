db82af67c6f167c9ab71660a2990bd5e4b68d5ec
Grim <mirgantrophy@gmail.com>
2016-11-29 19:08:08 -0600

    slight progress update..

--- src/com/mirgantrophy/audioengine/handler/AudioWatchDog.java
+++ src/com/mirgantrophy/audioengine/handler/AudioWatchDog.java
@@ -13,11 +13,13 @@ import javafx.scene.media.MediaPlayer;
 
 public class AudioWatchDog
 {
-	private MediaPlayer mediaPlayer;
-	private Media audio;
-	private String currentPath;
+	
+	//why not create a list for each mediaplayer
+	private MediaPlayer mediaPlayer; //plays our song
+	private Media audio; //contains the song we want to play
+	private String currentPath; //simply a pointer to the song we are currently referencing
 	private String[] pathList = {"/audio/01-opening-part-1-.mp3", "/audio/02-opening-part-2-.mp3", "/audio/03-pallet-town-s-theme.mp3", "/audio/04-professor-oak", "/audio/05-oak-research-lab.mp3"};
-	public AudioWatchDog(int index)
+	public AudioWatchDog(int index) //we make a new AWD which will play a song
 	{
 		System.out.println("setting path");
 		currentPath = AudioWatchDog.class.getResource(pathList[index]).toString();
@@ -30,7 +32,7 @@ public class AudioWatchDog
 	
 	public void setCurrentpath(int index)
 	{
-		currentPath = pathList[index];
+		currentPath = AudioWatchDog.class.getResource(pathList[index]).toString();
 	}
 	
 	public String getCurrentpath()
@@ -38,9 +40,34 @@ public class AudioWatchDog
 		return currentPath;
 	}
 	
-	public void Play()
+	public void changeMedia()
 	{
+		audio = null;
+		audio = new Media(currentPath);
+	}
+	
+	public Media getMedia()
+	{
+		return audio;
+	}
+	
+	public void play()
+	{
+		mediaPlayer = null;
+		mediaPlayer = new MediaPlayer(audio);
 		mediaPlayer.play();
 	}
 }
+
+/* 
+ * http://stackoverflow.com/questions/12482129/play-consecutive-videos-in-javafx
+ * https://gist.github.com/jewelsea/1446612
+ * Here is a sample program which plays an AudioPlaylist from the hard drive.
+
+The program works by creating a list containing a new MediaPlayer and a new Media for each file to be played. There is a single MediaView in the program and, for each MediaPlayer, onEndOfMedia the MediaView's MediaPlayer is set to the next one in the list. It's pretty much the same solution as jju's recursive answer.
+
+Note, to change the playing Media, you don't update the Media in the MediaPlayer (there is no way to do that as there is a 1:1 correspondence between Media and MediaPlayer), instead you update the MediaPlayer in the MediaView.
+
+The concept for Videos is pretty much the same as Audio because both Audio and Video in JavaFX are represented as Media played by a MediaPlayer and managed through a MediaView.
+ */
 
--- src/main/AudioEngine.java
+++ src/main/AudioEngine.java
@@ -13,7 +13,6 @@ import javafx.util.Duration;
 public class AudioEngine extends Application
 {
 	private static AudioWatchDog awd;
-	//private static MediaPlayer mediaPlayer; //this will get garbage collected if instantiated in the start method...
 	public static void main(String[] args) 
 	{	
 		launch(args);
@@ -23,130 +22,17 @@ public class AudioEngine extends Application
 	public void start(Stage primaryStage) throws Exception 
 	{	
 		awd = new AudioWatchDog(0);
-		awd.Play();
-		/*Scanner scan = new Scanner(System.in);
-		System.out.print("Please enter identifier: ");
-		String response = scan.nextLine();
-		switch(response)
+		awd.play();
+		Scanner scan = new Scanner(System.in);
+		int choice = scan.nextInt();
+		switch(choice)
 		{
-		case "01":
-			break;
-		case "02":
-			String path = AudioEngine.class.getResource("/audio/02-opening-part-2-.mp3").toString(); //use getResource for referencing items INSIDE
-			// a jar file.. NOTE: cannot run through the IDE this way! Needs to be compiled.
-			Media audio = new Media(path);
-			mediaPlayer = new MediaPlayer(audio);
-			mediaPlayer.setOnEndOfMedia(new Runnable() 
-			{
-				public void run()
-				{
-					mediaPlayer.seek(Duration.ZERO);
-				}
-			});
-			mediaPlayer.setAutoPlay(true); 
-			break;
-		case "03":
-			break;
-		case "04":
-			break;
-		case "05":
-			break;
-		case "06":
-			break;
-		case "07":
-			break;
-		case "08":
-			break;
-		case "09":
-			break;
-		case "10":
-			break;
-		case "11":
-			break;
-		case "12":
-			break;
-		case "13":
-			break;
-		case "14":
-			break;
-		case "15":
-			break;
-		case "16":
-			break;
-		case "17":
-			break;
-		case "18":
-			break;
-		case "19":
-			break;
-		case "20":
-			break;
-		case "21":
-			break;
-		case "22":
-			break;
-		case "23":
-			break;
-		case "24":
-			break;
-		case "25":
-			break;
-		case "26":
-			break;
-		case "27":
-			break;
-		case "28":
-			break;
-		case "29":
-			break;
-		case "30":
-			break;
-		case "31":
-			break;
-		case "32":
-			break;
-		case "33":
-			break;
-		case "34":
-			break;
-		case "35":
-			break;
-		case "36":
-			break;
-		case "37":
-			break;
-		case "38":
-			break;
-		case "39":
-			break;
-		case "40":
-			break;
-		case "41":
-			break;
-		case "42":
-			break;
-		case "43":
-			break;
-		case "44":
-			break;
-		case "45":
-			break;
-		case "46":
-			break;
-		case "47":
-			break;
-		case "48":
-			break;
-		case "49":
-			break;
-		case "50":
-			break;
-		case "51":
-			break;
-		default:
-			System.out.println("invalid identifier");
+		case 1:
+			awd.setCurrentpath(1);
+			awd.changeMedia();
+			awd.play();
 		}
-		System.out.println("Program Closing..."); */
+
 	} 
 }
 
@@ -156,4 +42,13 @@ String filePath = "audio/02-opening-part-2-.mp3";
 Media soundFile = new Media(new File(filePath).toURI().toString());
 mediaPlayer = new MediaPlayer(soundFile);
 mediaPlayer.play();
+
+			mediaPlayer.setOnEndOfMedia(new Runnable() 
+			{
+				public void run()
+				{
+					mediaPlayer.seek(Duration.ZERO);
+				}
+			});
+			mediaPlayer.setAutoPlay(true); 
 */
